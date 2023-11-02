40ca79f74e441b8e65b917b03e7842af2b6a122d
alpaBuddhabhatti <64379307+alpaBuddhabhatti@users.noreply.github.com>
2023-11-02 20:06:06 +0000

    Update Script.sql

--- Script.sql
+++ Script.sql
@@ -1,5 +1,9 @@
 -- Config Schema Creation
 --create schema config;
+
+drop table [config].[Pipeline_DeltaLoad]
+drop table [config].[Pipelines_Config]
+drop table [config].[PipelinesEmail_Config]
 --- Pipelines_Config
 CREATE TABLE config.Pipelines_Config (
  Id int NOT NULL identity Primary Key,
@@ -17,6 +21,8 @@ CREATE TABLE config.Pipelines_Config (
 );
 
 
+
+
 ---  Pipeline_DeltaLoad
 CREATE TABLE [config].[Pipeline_DeltaLoad] (
  [Id] INT IDENTITY (1, 1) NOT NULL,
@@ -31,7 +37,7 @@ VALUES ('BO001', '[config].[BO001_Goalscorers]', 'BO001_Goalscorers', 'ASQL', 'S
 INSERT INTO config.Pipelines_Config
 VALUES ('BO001', '[config].[BO001_Results]', 'BO001_Results', 'ASQL', 'SFTP', ' ', 'full', 'date,home_team,away_team,home_score,away_score', 'sftp', 'csv',0);
 INSERT INTO config.Pipelines_Config
-VALUES ('BO001', '[config].[BO001_Shootouts]', 'BO001_Shootouts', 'ASQL', 'SFTP', 'full', '*', '*', 'sftp', 'csv',0);
+VALUES ('BO001', '[config].[BO001_Shootouts]', 'BO001_Shootouts', 'ASQL', 'SFTP', ' ', 'full', '*', 'sftp', 'csv',0);
 INSERT INTO config.Pipelines_Config
 VALUES ('BO001', '[config].[BO001_History]', 'BO001_History', 'ASQL', 'ASQL', ' expireddate() ≤ (getdate()-90) ', 'delete', '*', '', '',0);
 
@@ -40,7 +46,7 @@ VALUES ('BO001', '[config].[BO001_History]', 'BO001_History', 'ASQL', 'ASQL', ' 
 INSERT INTO config.Pipelines_Config
 VALUES ('BO002', 'config.BO002_Deliveries', 'BO002_Deliveries', 'ASQL', 'ABLB', '', 'full', '*','cricket-in' ,'csv',0);
 INSERT INTO config.Pipelines_Config
-VALUES ('BO002', 'config.BO002_Matches', 'BO002_Matches', 'ASQL', 'ABLB', '', 'full', 'c1,c2,c3,c4','cricket-in','csv',0);
+VALUES ('BO002', 'config.BO002_Matches', 'BO002_Matches', 'ASQL', 'ABLB', '', 'full', 'season,team1,team2,date,match_number,venue,city,toss_winner,toss_decision,player_of_match,winner,winner_runs,winner_wickets','cricket-in','csv',0);
 INSERT INTO config.Pipelines_Config
 VALUES ('BO002', 'config.BO002_Pointstable', 'BO002_Pointstable', 'ASQL', 'ABLB', '', 'full', '*','cricket-in','csv',0);
 
@@ -48,14 +54,16 @@ VALUES ('BO002', 'config.BO002_Pointstable', 'BO002_Pointstable', 'ASQL', 'ABLB'
 -- BO003.sql  :Business Outcome 3 - Movie Config
  
 INSERT INTO config.Pipelines_Config
-VALUES('OB003', 'BO003_Movies', 'config.BO003_Movies', 'ABLB', 'ASQL', '', 'full', '*','', '',0);
+VALUES('BO003', 'BO003_Movies', 'config.BO003_Movies', 'ABLB', 'ASQL', '', 'full', '*','movie-in', '',0);
 
 --Delta Load for table2
 DECLARE @last_Inserted_Spec_Code_Id INT;
 INSERT INTO config.Pipelines_Config
-VALUES('OB003', 'BO003_Ratings','stage.BO003_Ratings', 'ABLB', 'ASQL','table_id', 'delta', '*', '','',0);
+VALUES('BO003', 'BO003_Ratings','stage.BO003_Ratings', 'ABLB', 'ASQL','table_id', 'delta', '*', 'movie-in','',0);
 set @last_Inserted_Spec_Code_Id = (SELECT SCOPE_IDENTITY());
 
+Insert into config.Pipeline_DeltaLoad(SpecCodeId) values (@last_Inserted_Spec_Code_Id)
+set @last_Inserted_Spec_Code_Id=0
 
 --Email Config
 
@@ -72,4 +80,7 @@ CREATE TABLE config.PipelinesEmail_Config (
 
 
 INSERT INTO config.PipelinesEmail_Config
-VALUES('OB003', 'meetalpa@gmail.com', 'meetalpa@gmail.com', '', 'BO003 has successfully run', 0, '');
+VALUES('BO003', 'meetalpa@gmail.com', 'meetalpa@gmail.com', '', 'BO003 has successfully run', 0, '');
+
+
+
